53e794fde9c75d2396c3cf087fb10fa5aa5a8d80
Tuk <hoatv.7604@gmail.com>
2024-09-18 17:57:36 +0700

    *1.0.0.8

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Cat Lucky-CatLucky-1.0.0.6";
+const cacheName = "Cat Lucky-CatLucky-1.0.0.8";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js.unityweb",
--- index.js
+++ index.js
@@ -40,7 +40,7 @@
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Cat Lucky",
     productName: "CatLucky",
-    productVersion: "1.0.0.6",
+    productVersion: "1.0.0.8",
     showBanner: unityShowBanner,
 	cacheControl: function (url) {
   //return "immutable";
