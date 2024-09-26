d2cda657be9f9702553fb9bbb13ac28113c0e9ac
Tuk <hoatv.7604@gmail.com>
2024-09-26 14:18:17 +0700

    *CB 1.0.12.11

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "CatB-Cat Battle-1.0.12.10";
+const cacheName = "CatB-Cat Battle-1.0.12.11";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js.unityweb",
--- ads.js
+++ ads.js
@@ -14,11 +14,49 @@
 			console.log("TUK isDone:" + result.done);
 			console.log("TUK JSON:" + JSON.stringify(result));
 			console.log("TUK JSON:" + JSON.stringify(result, null, 4));
+			if(unityInstanceRef)
+			{
+				unityInstanceRef.SendMessage("MegaADHandler", "OnRewardCompleted", JSON.stringify(data.type));
+			}
 			}).catch((result) => {
 				// user get error during playing ad or skip ad
 				// do nothing or whatever you want
 				console.log("TUK false:" + result);
-				SendMessage("MegaADHandler", "OnRewardCompleted", JSON.stringify(data.type));
+				if(unityInstanceRef)
+				{
+					unityInstanceRef.SendMessage("MegaADHandler", "OnRewardCompleted", JSON.stringify(data.type));
+				}
+		  })
+	  }
+  }
+  
+  function showReward(type)
+  {
+	  var data = JSON.parse(type);
+	  console.log("TUK data" + data.blockId);
+	  console.log("TUK data type" + data.type);
+	  const BannerAdController = window.Adsgram.init({ blockId: "3491" });
+	  if(BannerAdController)
+	  {
+		  BannerAdController.show().then((result) => {
+			// user watch ad till the end
+			// your code to reward user
+			console.log("TUK" + result);
+			console.log("TUK isDone:" + result.done);
+			console.log("TUK JSON:" + JSON.stringify(result));
+			console.log("TUK JSON:" + JSON.stringify(result, null, 4));
+			if(unityInstanceRef)
+			{
+				unityInstanceRef.SendMessage("MegaADHandler", "OnRewardCompleted", JSON.stringify(data.type));
+			}
+			}).catch((result) => {
+				// user get error during playing ad or skip ad
+				// do nothing or whatever you want
+				console.log("TUK false:" + result);
+				if(unityInstanceRef)
+				{
+					unityInstanceRef.SendMessage("MegaADHandler", "OnRewardCompleted", JSON.stringify(data.type));
+				}
 		  })
 	  }
   }
