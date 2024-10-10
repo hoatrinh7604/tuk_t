ece5bdf998b68fa3c679606ddebbce54a749a90f
Tuk <hoatv.7604@gmail.com>
2024-10-10 16:34:41 +0700

    *CB_1.0.13.27

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "CatB-Cat Battle-1.0.13.26";
+const cacheName = "CatB-Cat Battle-1.0.13.27";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js.unityweb",
