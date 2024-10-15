08ce9bbd5db990f6cb6af0c85a1d0c6c5fdb7083
Tuk <hoatv.7604@gmail.com>
2024-10-15 18:23:33 +0700

    *CatBattle_1.0.13.30

--- index.js
+++ index.js
@@ -93,7 +93,7 @@
     streamingAssetsUrl: "StreamingAssets",
     companyName: "CatB",
     productName: "Cat Battle",
-    productVersion: "1.0.13.29",
+    productVersion: "1.0.13.30",
     showBanner: unityShowBanner,
 	cacheControl: function (url) {
   //return "immutable";
