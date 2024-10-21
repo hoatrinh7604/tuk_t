ac951626ce98d753500ef74a4b694f1a416d4d19
Tuk <hoatv.7604@gmail.com>
2024-10-21 16:25:14 +0700

    *CBO_1.1.0.1

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "CatB-Cat Battle-1.0.15.1";
+const cacheName = "CatB-Cat Battle-1.1.0.1";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js.unityweb",
@@ -8,8 +8,6 @@ const contentToCache = [
     "index.js",
 	"index.html",
     "load-sdk.js",
-    "ads.js",
-    "tracking.js"
 ];
 
 // self.addEventListener('install', function(event) {
@@ -110,12 +108,6 @@ self.addEventListener("fetch", function (event) {
     // })()
   // );
   
-  // Only cache if it is game content data
-  if (!isStaticResource(event.request))
-  {
-	  return null;
-  }
-  
   //new
   if (event.request.method === 'POST') {
     // Always fetch from the network for POST requests
@@ -160,10 +152,6 @@ self.addEventListener('message', function(event) {
     }
 });
 
-// Helper function to check if the request is for static resources
-function isStaticResource(request) {
-  return contentToCache.some((resource) => request.url.includes(resource));
-}
 // Inform clients about the new version
 // self.addEventListener('activate', event => {
     // event.waitUntil(
