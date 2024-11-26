c6f47bdb962b2a43f593fe6d5e55056db0250a16
Tuk <hoatv.7604@gmail.com>
2024-11-26 14:33:09 +0700

    *CL

--- index.js
+++ index.js
@@ -67,7 +67,7 @@
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Cat Lucky",
     productName: "CatLucky",
-    productVersion: "1.0.2.8",
+    productVersion: "1.0.2.10",
     showBanner: unityShowBanner,
 	cacheControl: function (url) {
   //return "immutable";
